225cd5931dc18bfb8ba7efa209c0cb51c6e7b0a2
Liza Carter <lizahcarter@gmail.com>
2014-09-23 07:57:41 -0500

    added coveralls node module

--- client/views/happyhours/happyhours.js
+++ client/views/happyhours/happyhours.js
@@ -2,13 +2,12 @@
   'use strict';
 
   angular.module('do-werk')
-  .controller('HappyHoursCtrl', ['$scope', 'happyHour' ,function($scope, happyHour){
+  .controller('HappyHoursCtrl', ['$scope', 'happyHour', function($scope, happyHour){
     $scope.title = 'all happy hours';
     $scope.happyhours = [];
 
     happyHour.all().then(function(response){
       $scope.happyhours = response.data.happyhours;
-      console.log($scope.happyhours);
     });
   }]);
 })();
