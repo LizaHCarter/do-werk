bd075e10e986e7866afa856bf176368b87b326e4
Liza Carter <lizahcarter@gmail.com>
2014-10-08 12:13:22 -0500

    added register acceptance test

--- test/acceptance/happyhours.js
+++ test/acceptance/happyhours.js
@@ -27,6 +27,19 @@ describe('happyhours', function(){
       });
     });
   });
+  describe('post /register', function(){
+    it('should register a new user', function(done){
+      request(app)
+      .post('/register')
+      .send('email=sue@aol.com')
+      .send('password=1234')
+      .end(function(err, res){
+        cookie = res.headers['set-cookie'][0];
+        expect(res.status).to.equal(200);
+        done();
+      });
+    });
+  });
   describe('get /happyhours', function(){
     it('should show all happyhours page', function(done){
       request(app)
